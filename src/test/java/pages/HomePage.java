cd881a16590dd46363aeffe793d6a551be3aba7b
ana.mjeda <anamjeda@gmail.com>
2023-02-07 23:09:33 +0100

    added findby and method to HomePage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,11 +1,20 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage{
 
+    @FindBy (id = "btn-make-appointment")
+    private WebElement makeAppointmentButton;
+
     public HomePage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
+    }
+
+    public void clickMakeAppointmentButton(){
+        makeAppointmentButton.click();
     }
 }
