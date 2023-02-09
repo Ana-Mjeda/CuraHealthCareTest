9199acb7358a0df2c65d457f7897918164d95370
ana.mjeda <anamjeda@gmail.com>
2023-02-09 14:40:04 +0100

    added Assertations to check summarypage options in AppointmentTest

--- src/test/java/pages/SummaryPage.java
+++ src/test/java/pages/SummaryPage.java
@@ -5,21 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SummaryPage extends BasePage{
-
+public class SummaryPage extends BasePage {
 
     @FindBy(id = "facility")
     private WebElement facility;
 
-    @FindBy (id = "hospital_readmission")
+    @FindBy(id = "hospital_readmission")
     private WebElement readmission;
 
-    @FindBy (id = "program")
+    @FindBy(id = "program")
     private WebElement program;
 
-    @FindBy (id = "visit_date")
+    @FindBy(id = "visit_date")
     private WebElement visitDate;
-
 
     public SummaryPage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
--- src/test/java/tests/AppointmentTest.java
+++ src/test/java/tests/AppointmentTest.java
@@ -1,12 +1,13 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.SummaryPage;
 import pages.healthcareProgram;
 
-public class AppointmentTest extends BaseTest{
+public class AppointmentTest extends BaseTest {
 
     private SummaryPage summaryPage;
 
@@ -26,12 +27,17 @@ public class AppointmentTest extends BaseTest{
     }
 
     @Test
-    public void appointmentTest(){
+    public void appointmentTest() {
         String facility = "Hongkong CURA Healthcare Center";
         boolean applyCheckbox = true;
         healthcareProgram healthcareProgram = pages.healthcareProgram.None;
         String visitDate = "13/02/2023";
 
         appointmentPage.makeAppointment(facility, applyCheckbox, healthcareProgram, visitDate);
+        Assert.assertEquals(summaryPage.getFacility().getText(), facility);
+        Assert.assertEquals(summaryPage.getProgram().getText(), healthcareProgram.name());
+        Assert.assertEquals(summaryPage.getVisitDate().getText(), visitDate);
+        Assert.assertEquals(summaryPage.getReadmission().getText(), "Yes");
+
     }
 }
